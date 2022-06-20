4c16b00dc17c11cdfe3bdcd274fe2385daa5b770
D1_62973_Archyasome <sarkar.archyasome97@gmail.com>
2022-06-20 16:50:01 +0530

    isEven Function added

---
+++ page1.js
@@ -0,0 +1,10 @@
+function isEven(number){
+    if(number%2==0)
+    {
+        console.log(`${number} is EVEN`)
+    }else{
+        console.log(`${number} is notEVEN`)
+    }
+}
+isEven(10)
+isEven(13)
